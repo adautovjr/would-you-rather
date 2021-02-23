dffc98bea2e55d3fe7cccdb8324b65acc724c81f
Adauto Guedes <adauto.guedes@fiocruz.br>
2021-02-23 16:16:13 -0300

    Hotfix: Remove unused component

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { CookiesProvider } from 'react-cookie';
 import createRootReducer from './reducers';
 import middleware, { history } from './middleware';
 export const store = createStore(createRootReducer(history), middleware);
 
 ReactDOM.render(
     <Provider store={store}>
-        <CookiesProvider>
-            <App />
-        </CookiesProvider>
+        <App />
     </Provider>,
     document.getElementById('root')
 );
